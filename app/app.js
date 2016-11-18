422d44369f79f623b75fcb50f3919558f71f845b
Apoclyps <kyle90adam@hotmail.com>
2016-11-18 22:25:41 +0000

    Adding Contact feature to UI

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular
     .module('myApp', [
         'ngRoute',
         'myApp.home',
+        'myApp.contact',
         'myApp.help',
         'myApp.organisations',
         'myApp.volunteers',
